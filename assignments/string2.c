14216275a56def65e864592764bf3becf0e902d1
hasleenkaur <h.kaur@gobindsarvar.ca>
2025-03-26 18:14:41 +0000

    string2

---
+++ assignments/string2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char greetings[] = {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0'};
+    printf("%s\n", greetings);
+    printf("%llu\n", strlen(greetings));
+  
+    char message[] = "Good to see you,";
+    char f_name[] = "John";
+    printf("%s %s!\n", message, f_name);
+  
+    return 0;
+  }
